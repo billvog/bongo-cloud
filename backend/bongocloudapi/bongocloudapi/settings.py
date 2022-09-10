17eff16427446947a8cc0a970193318629ab8f5f
billvog <billvog04@gmail.com>
2022-09-10 18:55:39 +0300

    Added and configured django-cors-headers

--- backend/bongocloudapi/bongocloudapi/settings.py
+++ backend/bongocloudapi/bongocloudapi/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -43,6 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -132,6 +134,9 @@ MEDIA_ROOT = 'bongo-storage/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Cors
+CORS_ALLOWED_ORIGINS = ['http://localhost:3000']
 
 # Django Rest Framework
 REST_FRAMEWORK = {
