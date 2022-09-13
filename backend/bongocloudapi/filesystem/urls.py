ef1094f75677903a7eacda2c160607af1f1c5bec
billvog <billvog04@gmail.com>
2022-09-13 13:41:20 +0300

    Added trailing slash at all urls

--- backend/bongocloudapi/filesystem/urls.py
+++ backend/bongocloudapi/filesystem/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
   path('', views.FilesystemItemListAPIView.as_view()),
-  path('create', views.FilesystemCreateAPIView.as_view()),
+  path('create/', views.FilesystemCreateAPIView.as_view()),
   path('<uuid:pk>/', views.FilesystemItemListAPIView.as_view()),
-  path('<uuid:pk>/update', views.FilesystemItemUpdateAPIView.as_view()),
-  path('<uuid:pk>/info', views.FilesystemItemRetrieveDestroyAPIView.as_view()),
-  path('<uuid:pk>/delete', views.FilesystemItemRetrieveDestroyAPIView.as_view()),
+  path('<uuid:pk>/update/', views.FilesystemItemUpdateAPIView.as_view()),
+  path('<uuid:pk>/info/', views.FilesystemItemRetrieveDestroyAPIView.as_view()),
+  path('<uuid:pk>/delete/', views.FilesystemItemRetrieveDestroyAPIView.as_view()),
 ]
