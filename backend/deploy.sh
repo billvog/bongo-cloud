4cc74f6df455ce3dbcba33fbee18f26116aa5447
billvog <billvog04@gmail.com>
2022-09-16 14:14:24 +0300

    Added deploy script for backend

---
+++ backend/deploy.sh
@@ -0,0 +1,10 @@
+echo 'Input version for docker image:'
+read version
+
+docker_image=billvog/bongocloudapi:$version
+
+echo 'Status: Building' $docker_image '...'
+docker buildx build --platform linux/amd64 -t $docker_image . --load
+
+echo 'Status: Image built, copying it over SSH to billvog@bongoapi.bongo-cloud.ga ...'
+docker save $docker_image | ssh -C -i ~/.ssh/id_rsa_voyager2 -p 8678 billvog@bongoapi.bongo-cloud.ga "docker load"
