bf8cf783e6ed041dffe8051549fc1a764a2e7d31
billvog <billvog04@gmail.com>
2022-09-22 19:22:16 +0300

    Added gunicorn

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -9,4 +9,4 @@ echo 'Status: Building' $docker_image '...'
 docker buildx build --platform linux/amd64 -t $docker_image . --load
 
 echo 'Status: Image built, copying it over SSH to billvog@bongoapi.bongo-cloud.ga ...'
-docker save $docker_image | ssh -C -i ~/.ssh/id_rsa_voyager2 -p 8678 billvog@bongoapi.bongo-cloud.ga "docker load"
+docker save $docker_image | ssh -C -i ~/.ssh/id_rsa_voyager2 -p 8678 billvog@bongoapi.bongo-cloud.ga "echo \"Status: Loading image...\" && docker load"
---
+++ backend/migrate.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /code/bongocloudapi
+python manage.py migrate --noinput
